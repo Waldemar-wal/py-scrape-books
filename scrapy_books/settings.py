dc9b3ad0192738419cc0f011055f68f0e9070da1
Vova Savenkov <sava.v80@gmail.com>
2025-06-25 17:52:43 +0300

    fix

--- scrapy_books/settings.py
+++ scrapy_books/settings.py
@@ -46,15 +46,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "scrape_books.middlewares.ScrapeBooksSpiderMiddleware": 543,
-#}
+SPIDER_MIDDLEWARES = {
+    "scrapy_books.middlewares.ScrapyBooksSpiderMiddleware": 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "scrape_books.middlewares.ScrapeBooksDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    "scrapy_books.middlewares.ScrapyBooksDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "scrape_books.pipelines.ScrapeBooksPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "scrapy_books.pipelines.ScrapyBooksPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
